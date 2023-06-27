63cf9ac7d7be65280f24d89134452443c1aec2cc
syed <israr_husain@yahoo.com>
2023-06-27 14:28:24 +0500

    list view builder and hero tag

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'movie_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Movie list",
+        theme: ThemeData(primarySwatch: Colors.green),
+        debugShowCheckedModeBanner: false,
+        // home : new ListViewBuilder(), NO Need To Use Unnecessary New Keyword
+        home: const MovieScreen());
+  }
+}
